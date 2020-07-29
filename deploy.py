773d85fe849c12256fd2705ba2fc95f634d85652
Parth Shah <parthjshah95@gmail.com>
2020-07-29 14:51:58 -0400

    released_on_2020-07-29 14:50:47.491745

---
+++ deploy.py
@@ -0,0 +1,33 @@
+import os
+import datetime
+import shutil
+
+def done():
+    print("done")
+
+def msg(message):
+    print(dash, message, dots, dash)
+
+now = str(datetime.datetime.now())
+dash = "\n-----------------------------\n"
+dots = "..."
+
+msg("cleaning old build files")
+shutil.rmtree('docs', ignore_errors=True)
+done()
+
+msg("building source")
+os.system("npm run build")
+done()
+
+msg("preparing docs")
+os.rename('dist/', 'docs/')
+done()
+
+msg("deploying to github pages")
+os.system("git add .\\docs\\")
+os.system("git commit -m 'released_on_"+now+"'")
+os.system("git push")
+done()
+
+msg("completed deployment process")
