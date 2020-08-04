2b4fc8bef3e706790bab4733cf61729e29b6eeb8
Parth Shah <parthjshah95@gmail.com>
2020-08-03 20:24:32 -0400

    added fixed image, scroll down nudge, animate on scroll and draft experience card

---
+++ src/components/ExperienceCard.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="card" data-aos="zoom-in">
+        <img class="icon" src="../assets/UF_logo.jpg">
+        <div class="info">
+            <div class="position">Research Assistant</div>
+            <div class="institute"><b>University of Florida</b></div>
+            <div class="description">Modern Artificial intelligence and Learning Technologies (MALT) lab</div>
+            <div class="time"><b><i>Oct 2019 - Present</i></b></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+$card-height: 100px;
+.card{
+    margin:10px;
+    border-radius: 10px;
+    box-shadow: 0 5px 20px rgba(0,0,0,0.19), 0 3px 6px rgba(0,0,0,0.23);
+    overflow: hidden;
+    height: $card-height;
+}
+.icon{
+    position:absolute;
+    left: 0px;
+    width: $card-height;
+    height: $card-height;
+}
+$margin:10px;
+.info{
+    position: absolute;
+    left: $card-height;
+    height: $card-height - 20px;
+    width: calc(100% - #{$card-height+2*$margin});
+    margin: $margin;
+    text-align: left;
+}
+.position{
+    font-size: 1.4rem;
+    color: #557;
+}
+.institute{
+    color: #f7700f;
+    margin-top: 5px;
+    margin-bottom: 7px;
+    text-transform: uppercase;
+}
+.time{
+    position: absolute;
+    right: 5px;
+    top: 5px;
+    font-size: 1.0rem;
+    color: #f7700f;
+    text-transform: uppercase;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,45 +5,57 @@
       <div class="image-overlay"></div>
       <img class="photo" src="../assets/me.jpg">
       <h1>PARTH SHAH</h1>
-      <h4>Full stack | Machine learning | Android</h4>
+      <h4>Full stack | Machine learning </h4>
+      <iframe class="down" src="https://giphy.com/embed/UrzWDQ3VTiDU84R5dx"></iframe>
     </div>
-    <h2>Foo bar</h2>
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div class="bg-wh">
+      <h2>Experience</h2>
+      <ExperienceCard></ExperienceCard>
+      <!-- <div class="container temp" data-aos="zoom-in">
+        lorem ipsum dolor si amet
+      </div> -->
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
+import ExperienceCard from '@/components/ExperienceCard.vue'
 
 export default {
   name: 'Home',
   components: {
-    // HelloWorld
+    // HelloWorld,
+    ExperienceCard
   }
 }
 </script>
 
 <style lang="scss" scoped>
+html {
+  overflow-x: hidden;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
 .cont-bleed{
   width:100%;
   height: 100vh;
   overflow: hidden;
+  z-index:0;
 }
 .image-bleed {
   height: 100%;
   width: 100%;
   object-fit: cover;
   z-index: -2;
-  position:absolute;
+  position:fixed;
   left: 0px;
   top:0px;
 }
 .image-overlay {
   @extend .image-bleed;
   z-index: -1;
-  background-color: rgba(64, 55, 105, 0.8)
+  background-color: rgba(64, 55, 105, 0.85)
 }
 
 $photo-size: 200px;
@@ -60,9 +72,20 @@ $photo-size: 200px;
     transform: scale(1.05);
   }
 }
+$arrow-size: 30px;
+.down{
+  position: fixed;
+  bottom:0px;
+  left: calc(50vw - #{($arrow-size / 2)});
+  width: $arrow-size;
+  height: $arrow-size;
+  z-index: -1;
+}
+iframe{
+  border:none;
+}
 h1 {
-  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
-  font-size: 4rem;
+  font-size: 3rem;
   color: white;
   font-style: bold;
   font-variant: normal;
@@ -71,9 +94,25 @@ h1 {
 }
 h4{
   @extend h1;
-  font-family: "Arial Black", Helvetica, sans-serif;
   margin-top:0px;
   font-size: 1.8rem;
   font-weight: 500;
+}
+h2{
+  @extend h4;
+  color: #444;
+  margin: 20px;
+  // padding: 10px;
+}
+.bg-wh{
+  background-color: white;
+  margin: 0px;
+  padding: 20px;
+  z-index: 1;
+  height: 500px;
+}
+.temp{
+  width: 700px;
+  height: 30px;
 }
 </style>
